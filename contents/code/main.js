48c4fa3840dd1765c5ba8f3dc9418bf1a8581184
jofi <jf505@gmx.de>
2025-10-01 17:11:28 +0200

    included location configuration

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,27 +1,34 @@
 
-const wallpapers = [
-    "/usr/share/wallpapers/Foreland",
-    "/home/texter/.local/share/wallpapers/A Small Tree"
-];
+const default_wallpaper = "/home/texter/.local/share/wallpapers/A Small Tree";
+const wallpapers = {
+    3: "/home/texter/.local/share/wallpapers/Technology",
+    4: "/home/texter/.local/share/wallpapers/Skylines",
+    5: "/home/texter/.local/share/wallpapers/Bonsai"
+};
+
+const default_location = "/home/texter/Desktop";
+const locations = {
+    5: "/home/texter/Documents/Programming/Plasma"
+};
 
 workspace.currentDesktopChanged.connect(function() {
-    // print("Desktop changed to desktop " + workspace.currentDesktop.x11DesktopNumber);
-    desktop_number = workspace.currentDesktop.x11DesktopNumber - 1;
-    if (desktop_number >= wallpapers.length) {
-        return;
-    }
-    set_wallpaper = `
-        var d = desktops()[0];
+    desktop_number = workspace.currentDesktop.x11DesktopNumber;
+    const wallpaper = (desktop_number in wallpapers) ? wallpapers[desktop_number] : default_wallpaper;
+    const location = (desktop_number in locations) ? locations[desktop_number] : default_location;
+    configure = `
+        var d = desktopForScreen(0);
+        d.currentConfigGroup = ["General"];
+        d.writeConfig("url", "${location}");
         d.wallpaperPlugin = "org.kde.image";
-        d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");
-        d.writeConfig("Image", "${wallpapers[desktop_number]}");`;
-
+        d.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"];
+        d.writeConfig("Image", "${wallpaper}");
+    `;
     callDBus(
         "org.kde.plasmashell",
         "/PlasmaShell",
         "org.kde.PlasmaShell",
         "evaluateScript",
-        set_wallpaper
+        configure
     );
 });
 
