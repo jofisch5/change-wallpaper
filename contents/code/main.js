3d2be4239fb91e27fd60935bedb330db4ec3abf8
jofi <jf505@gmx.de>
2025-10-30 14:43:57 +0100

    changes in plasma 6.5 - now the wallpaper always follows the plasma style even if the settings says always show light so i had to adapt my script to follow the color scheme - we read the scheme out of the config file and we have to go directly to the wallpaper image name which also can be different

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,9 +1,11 @@
 
-const default_wallpaper = "/home/texter/.local/share/wallpapers/A Small Tree";
+const base_path = "/home/texter/.local/share/wallpapers/";
+const default_wallpaper = {name: "A Small Tree", image: {light: "2560x1440.jpg", dark: "2560x1440.jpg"}};
+const image_prefix = "/contents/images";
 const wallpapers = {
-    3: "/home/texter/.local/share/wallpapers/Technology",
-    4: "/home/texter/.local/share/wallpapers/Skylines",
-    5: "/home/texter/.local/share/wallpapers/Bonsai"
+    3: {name: "Technology", image: {light: "2560x1440.jpg", dark: "3840x2160.jpg"}},
+    4: {name: "Skylines", image: {light: "2560x1440.jpg", dark: "2560x1440.jpg"}},
+    5: {name: "Bonsai", image: {light: "2560x1440.jpg", dark: "2560x1440.jpg"}}
 };
 
 const default_location = "/home/texter/Desktop";
@@ -11,9 +13,16 @@ const locations = {
     5: "/home/texter/Documents/Programming/Plasma"
 };
 
-workspace.currentDesktopChanged.connect(function() {
-    desktop_number = workspace.currentDesktop.x11DesktopNumber;
+function buildImagePath(desktop_number, color) {
     const wallpaper = (desktop_number in wallpapers) ? wallpapers[desktop_number] : default_wallpaper;
+    const isLight = color.endsWith("Light");
+    const image_name = isLight ? wallpaper.image.light : wallpaper.image.dark;
+    const suffix = isLight ? "/" : "_dark/";
+    return base_path + wallpaper.name + image_prefix + suffix + image_name;
+}
+
+function updateConfiguration(desktop_number, color) {
+    const image = buildImagePath(desktop_number, color);
     const location = (desktop_number in locations) ? locations[desktop_number] : default_location;
     configure = `
         var d = desktopForScreen(0);
@@ -21,7 +30,7 @@ workspace.currentDesktopChanged.connect(function() {
         d.writeConfig("url", "${location}");
         d.wallpaperPlugin = "org.kde.image";
         d.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"];
-        d.writeConfig("Image", "${wallpaper}");
+        d.writeConfig("Image", "${image}");
     `;
     callDBus(
         "org.kde.plasmashell",
@@ -30,6 +39,23 @@ workspace.currentDesktopChanged.connect(function() {
         "evaluateScript",
         configure
     );
+}
+
+let readColorScheme = `
+    const config = ConfigFile('kdeglobals');
+    config.group = 'General';
+    print(config.readEntry('ColorScheme'));
+`
+workspace.currentDesktopChanged.connect(function() {
+    desktop_number = workspace.currentDesktop.x11DesktopNumber;
+    callDBus(
+        "org.kde.plasmashell",
+        "/PlasmaShell",
+        "org.kde.PlasmaShell",
+        "evaluateScript",
+        readColorScheme,
+        function (color) {
+            updateConfiguration(desktop_number, color);
+        }
+    );
 });
-
-
